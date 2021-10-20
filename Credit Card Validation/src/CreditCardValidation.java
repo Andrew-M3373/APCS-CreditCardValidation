e39d2eb2dbc8e1aa361bff0bb2e9f86b9d64f581
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-10-20 12:15:01 -0600

    added more methods

--- Credit Card Validation/src/CreditCardValidation.java
+++ Credit Card Validation/src/CreditCardValidation.java
@@ -9,22 +9,34 @@ public class CreditCardValidation {
 	public static void main(String[] args) throws IOException {
 
 		Scanner file = new Scanner(new File("CardNumbers"));
-//		while (file.hasNextLine()) {
-			validateCard(file);
-//		}
+		while (file.hasNextLine()) {
+			validateCardsInFile(file);
+		}
+
+		System.out.println("There are " + validSum + " valid card numbers in the document.");
 		
+		System.out.println("\nWhat do you want to do?"
+				+ "\n1.\tGenerate 100 valid card numbers"
+				+ "\n2.\tCheck if your number is valid"
+				+ "\n3.\tExit");
+		Scanner userInput = new Scanner(System.in);
+		int input = userInput.nextInt();
+		if (input == 2) {
+//			validateInput();
+		}
+		else if (input == 1) {
+			generate100();
+		}
 	}
 	
-	public static void validateCard(Scanner f) {
+	public static void validateCardsInFile(Scanner f) {
+		
+		String wholeNumber = f.nextLine();
 		int[] number = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
-//		for (int i = 0; i < 16; i++) {
-//			number[i] = Integer.parseInt(f.next());
-//		}
-//		f.next();
-		for (Integer i: number) {
-			System.out.print(i);
+				//Integer.parseInt(f.nextLine().split(""));
+		for (int i = 0; i < wholeNumber.length(); i++) {
+			number[i] = Integer.parseInt(wholeNumber.substring(i, i+1));
 		}
-		System.out.println();
 		
 		for (int i = 0; i < 16; i++) {
 			if (i % 2 == 0) {
@@ -42,8 +54,36 @@ public class CreditCardValidation {
 		if (sum % 10 == 0) {
 			validSum ++;
 		}
-		System.out.println(validSum);
 		
+	}
+	
+	public static void generate100() {
+		int[] number = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
+		for (int i = 0; i < 100;) {
+			for (int j = 0; j < 16; j++) {
+				int random = (int) (Math.random() * 10);
+				number[j] = random;
+			}
+			for (int j = 0; j < 16; j++) {
+				if (j % 2 == 0) {
+					number[j] = number[j]*2;
+					if (number[j] >= 10) {
+						number[j] = number[j] % 10 + number[j] / 10;
+					}
+				}
+			}
+			int sum = 0;
+			for (int j = 0; j < 16; j++) {
+				sum += number[j];
+			}
+			if (sum % 10 == 0) {
+				i++;
+				for (int k = 0; k < 16; k++) {
+					System.out.print(number[k]);
+				}
+				System.out.println();
+			}
+		}
 	}
 
 }
