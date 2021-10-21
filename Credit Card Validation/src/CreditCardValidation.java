5d4fc2ea3cb2a7d85f295cdd396913bac8777d35
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-10-21 11:40:21 -0600

    finished 100 generation, work on number validation

--- Credit Card Validation/src/CreditCardValidation.java
+++ Credit Card Validation/src/CreditCardValidation.java
@@ -22,7 +22,7 @@ public class CreditCardValidation {
 		Scanner userInput = new Scanner(System.in);
 		int input = userInput.nextInt();
 		if (input == 2) {
-//			validateInput();
+			validateInput(userInput);
 		}
 		else if (input == 1) {
 			generate100();
@@ -32,7 +32,7 @@ public class CreditCardValidation {
 	public static void validateCardsInFile(Scanner f) {
 		
 		String wholeNumber = f.nextLine();
-		int[] number = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
+		int[] number = new int[16];
 				//Integer.parseInt(f.nextLine().split(""));
 		for (int i = 0; i < wholeNumber.length(); i++) {
 			number[i] = Integer.parseInt(wholeNumber.substring(i, i+1));
@@ -57,8 +57,12 @@ public class CreditCardValidation {
 		
 	}
 	
-	public static void generate100() {
-		int[] number = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
+	public static void generate100() throws IOException {
+		int[] number = new int[16];
+		File f = new File("generated");
+		FileWriter fw = new FileWriter(f, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
 		for (int i = 0; i < 100;) {
 			for (int j = 0; j < 16; j++) {
 				int random = (int) (Math.random() * 10);
@@ -79,11 +83,42 @@ public class CreditCardValidation {
 			if (sum % 10 == 0) {
 				i++;
 				for (int k = 0; k < 16; k++) {
-					System.out.print(number[k]);
+					pw.print(number[k]);
 				}
-				System.out.println();
+				pw.println();
 			}
 		}
+		pw.close();
+		bw.close();
+		fw.close();
+		System.out.println("\n100 potentially valid credit card numbers have been appended to the \"generated\" text file.");
 	}
 
+	public static void validateInput(Scanner ui) {
+		int[] number = new int[16];
+
+		System.out.println("Enter the card number to validate.");
+		Scanner userInput = new Scanner(System.in);
+		String input = userInput.nextLine();
+		for (int i = 0; i < input.length(); i++) {
+			number[i] = Integer.parseInt(input.substring(i, i+1));
+		}
+		
+		int[] original = number;
+		for (int j = 0; j < 16; j++) {
+			if (j % 2 == 0) {
+				number[j] = number[j]*2;
+				if (number[j] >= 10) {
+					number[j] = number[j] % 10 + number[j] / 10;
+				}
+			}
+		}
+		int sum = 0;
+		for (int j = 0; j < 16; j++) {
+			sum += number[j];
+		}
+		if (sum % 10 == 0) {
+			System.out.print("\nYour card number " + input + " is valid.\n");
+		}
+	}
 }
